f2cc60dab108e338dac37411f49eeec7cc1b4fa0
Alexander Petkov <sashko506@gmail.com>
2024-04-28 20:29:15 +0300

    fix: Set API_URL Some sort of conflict was observed with BASE_URL. Save API_URL in its own env variable

--- src/api/apiClient.ts
+++ src/api/apiClient.ts
@@ -1,3 +1,3 @@
 import axios from "axios";
 
-export const apiClient = axios.create({ baseURL: import.meta.env.BASE_URL });
+export const apiClient = axios.create({ baseURL: import.meta.env.API_URL });
