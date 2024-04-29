3765820fec51e48ffe0c3facc069049735085689
Alexander Petkov <sashko506@gmail.com>
2024-04-29 03:03:02 +0300

    build: Tanstack Query setup

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import { RouterProvider, createRouter } from "@tanstack/react-router";
 import { routeTree } from "./routeTree.gen";
 import { ThemeProvider } from "@emotion/react";
 import theme from "./common/theme";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 // Create a new router instance
 const router = createRouter({ routeTree });
@@ -18,14 +19,17 @@ declare module "@tanstack/react-router" {
 }
 
 // Render the app
+const queryClient = new QueryClient();
 const rootElement = document.getElementById("root")!;
 if (!rootElement.innerHTML) {
   const root = ReactDOM.createRoot(rootElement);
   root.render(
     <StrictMode>
-      <ThemeProvider theme={theme}>
-        <RouterProvider router={router} />
-      </ThemeProvider>
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider theme={theme}>
+          <RouterProvider router={router} />
+        </ThemeProvider>
+      </QueryClientProvider>
     </StrictMode>
   );
 }
