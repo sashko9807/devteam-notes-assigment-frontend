bae51a89ff38a2ac4b5a88ec4f9dbc005a1a866c
Alexander Petkov <sashko506@gmail.com>
2024-05-03 14:58:25 +0300

    chore: entry-client cleanup

--- src/entry-client.tsx
+++ src/entry-client.tsx
@@ -10,7 +10,6 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const router = createRouter();
 
-let root: ReturnType<typeof ReactDOM.createRoot>;
 const cache = createEmotionCache();
 const queryClient = new QueryClient();
 
@@ -26,32 +25,4 @@ function App() {
   );
 }
 
-router.hydrate();
-React.startTransition(() => {
-  root = ReactDOM.hydrateRoot(document, <App />);
-
-  if (root) {
-    //
-  }
-});
-
-// This is a workaround for react hydration completely barfing when
-// browser extensions mutate the DOM before hydration
-// This is a temporary solution until React fixes this issue
-// const checker = setInterval(() => {
-//   if (!document.documentElement) {
-//     console.warn(
-//       "We detected that browser extensions mutated the DOM before hydration and caused some hydration errors. Falling back to client-side rendering."
-//     );
-//     clearInterval(checker);
-//     clearTimeout(checkedOut);
-//     // const htmlEl = document.createElement('html')
-//     // htmlEl.innerHTML = '<head></head><body></body>'
-//     // document.replaceChildren(htmlEl)
-//     root.render(<App />);
-//   }
-// }, 1);
-
-// const checkedOut = setTimeout(() => {
-//   clearInterval(checker);
-// }, 1000);
+ReactDOM.hydrateRoot(document, <App />);
