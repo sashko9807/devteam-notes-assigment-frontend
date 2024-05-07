fc7a2728d47fc2c44d2eba5a0539d4f859505736
Alexander Petkov <sashko506@gmail.com>
2024-05-07 15:27:32 +0300

    ui: Add initial typography schema

--- src/common/theme.ts
+++ src/common/theme.ts
@@ -1,6 +1,9 @@
 import { createTheme } from "@mui/material";
 import { colors } from "@mui/material";
-
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 // A custom theme for this app
 const theme = createTheme({
   palette: {
@@ -12,6 +15,29 @@ const theme = createTheme({
     },
     error: {
       main: colors.red.A400,
+    },
+  },
+  typography: {
+    h1: {
+      fontSize: "4.5rem",
+    },
+    h2: {
+      fontSize: "3rem",
+    },
+    h3: {
+      fontSize: "2.074rem",
+    },
+    h4: {
+      fontSize: "1.728rem",
+    },
+    h5: {
+      fontSize: "1.44rem",
+    },
+    h6: {
+      fontSize: "1.2rem",
+    },
+    body1: {
+      fontSize: "1rem",
     },
   },
 });
